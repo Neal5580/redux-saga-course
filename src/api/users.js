e9197eda67d6424f41116b288114af2828d9403f
Neal Shen <shenyijie5580@gmail.com>
2019-03-06 07:56:23 +1100

    added create user redux saga

--- src/api/users.js
+++ src/api/users.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 export const getUsers = () => {
-    return axios.get("/users", {
-        params: {
-            limit: 1000
-        }
-    });
+  return axios.get("/users", {
+    params: {
+      limit: 1000
+    }
+  });
+};
+
+export const createUser = ({ firstName, lastName }) => {
+  return axios.post("/users", {
+    firstName,
+    lastName
+  });
 };
--- src/components/App.js
+++ src/components/App.js
@@ -5,32 +5,31 @@ import UsersList from "./UsersList";
 import NewUserForm from "./NewUserForm";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.props.getUserRequest();
-    }
+  constructor(props) {
+    super(props);
+    this.props.getUserRequest();
+  }
 
-    handleSubmit = ({ firstName, lastName }) => {
-        console.log(firstName, lastName);
-        this.props.createUserRequest({
-            firstName,
-            lastName
-        });
-    };
-    render() {
-        const users = this.props.users;
-        return (
-            <div
-                style={{ margin: "0 auto", padding: "20px", maxWidth: "600px" }}
-            >
-                <NewUserForm onSubmit={this.handleSubmit} />
-                <UsersList users={users.items} />
-            </div>
-        );
-    }
+  handleSubmit = ({ firstName, lastName }) => {
+    console.log(firstName, lastName);
+    this.props.createUserRequest({
+      firstName,
+      lastName
+    });
+  };
+
+  render() {
+    const users = this.props.users;
+    return (
+      <div style={{ margin: "0 auto", padding: "20px", maxWidth: "600px" }}>
+        <NewUserForm onSubmit={this.handleSubmit} />
+        <UsersList users={users.items} />
+      </div>
+    );
+  }
 }
 
 export default connect(
-    ({ users }) => ({ users }),
-    { getUserRequest, createUserRequest }
+  ({ users }) => ({ users }),
+  { getUserRequest, createUserRequest }
 )(App);
